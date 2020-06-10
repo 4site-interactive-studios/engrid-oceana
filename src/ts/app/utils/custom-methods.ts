c5cf4492837e33f24e155189450ac391017e6d2f
bryancasler <bryan.casler@gmail.com>
2020-06-09 23:43:42 -0400

    Working on a more robust targeting of this field

--- src/ts/app/utils/custom-methods.ts
+++ src/ts/app/utils/custom-methods.ts
@@ -47,6 +47,7 @@ export const enInput = (() => {
               targetWrapper.classList.remove("en__field__item--hidden");
               if (targetWrapper.parentNode) {
                 const lastRadioInput = targetWrapper.parentNode.querySelector(
+                  // "[name='transaction.donationAmt'][value='other' i]"
                   ".en__field__item:nth-last-child(2) input"
                 ) as HTMLInputElement;
                 lastRadioInput.checked = !0;
