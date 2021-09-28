7fee59e03f12a871ef3a60a36d9ef10c796e007d
maansacdalan <maansacdalan@gmail.com>
2021-09-27 20:17:36 -0400

    moved css

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -2,7 +2,7 @@
  * *************************************************
  * ENGRID PAGE TEMPLATE ASSETS
  *
- * Date: Monday, September 27, 2021 @ 14:37:30 ET
+ * Date: Monday, September 27, 2021 @ 20:15:48 ET
  * By: maansacdalan
  * ENGrid styles: vTBD1
  * ENGrid scripts: vTBD2
