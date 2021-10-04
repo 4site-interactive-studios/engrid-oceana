7cf732fe228cee5be34b1ffd59c30d080bd65ea3
maansacdalan <maansacdalan@gmail.com>
2021-10-04 16:58:35 -0400

    css cleanup to templates

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Monday, October 4, 2021 @ 12:39:30 ET
+ *  Date: Monday, October 4, 2021 @ 16:55:44 ET
  *  By: maansacdalan
  *  ENGrid styles: v0.3.38
  *  ENGrid scripts: v0.3.38
