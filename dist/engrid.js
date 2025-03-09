945be431a161b25cc1b78291995d2dce761ba9f0
Bryan Casler <bryan.casler@gmail.com>
2025-03-09 17:44:42 -0400

    Add styles for displaying based on theme version and to hide ad images

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Sunday, March 9, 2025 @ 16:13:14 ET
+ *  Date: Sunday, March 9, 2025 @ 17:44:19 ET
  *  By: 4Site
  *  ENGrid styles: v0.20.9
  *  ENGrid scripts: v0.20.10
