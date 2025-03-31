334f136391b4e915c87ac82b3a6ac7b154b851b5
Bryan Casler <bryan.casler@gmail.com>
2025-03-31 00:01:52 -0400

    Fixed background resizing issue

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Sunday, March 30, 2025 @ 23:22:30 ET
+ *  Date: Sunday, March 30, 2025 @ 23:59:07 ET
  *  By: 4Site
  *  ENGrid styles: v0.20.9
  *  ENGrid scripts: v0.20.10
