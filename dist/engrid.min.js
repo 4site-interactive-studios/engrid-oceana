1eb882c0200fb68589c73136d7d2d825f8e6aec6
Bryan Casler <bryan.casler@gmail.com>
2025-04-04 10:02:38 -0400

    Mobile Background Image to Banner Image Polish

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Friday, April 4, 2025 @ 09:54:27 ET
+ *  Date: Friday, April 4, 2025 @ 10:01:57 ET
  *  By: 4Site
  *  ENGrid styles: v0.20.9
  *  ENGrid scripts: v0.20.10
