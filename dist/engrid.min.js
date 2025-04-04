92be18985a8f47a79fd1f99ecbdbcad12315b53d
bryancasler <bryan.casler@gmail.com>
2025-04-04 14:00:08 -0400

    Fixed background to mobile banner image height

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Friday, April 4, 2025 @ 12:53:52 ET
+ *  Date: Friday, April 4, 2025 @ 12:57:25 ET
  *  By: bryancasler
  *  ENGrid styles: v0.21.0
  *  ENGrid scripts: v0.21.0
