d3c3e6f7c2651b5900c2e4e37d7f4338a59b62cc
Daryl Brewer <brewguy@gmail.com>
2025-06-12 13:22:58 -0400

    removed extra whitespace for premium gift description

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, June 12, 2025 @ 11:24:54 ET
+ *  Date: Thursday, June 12, 2025 @ 13:22:16 ET
  *  By: daryl
  *  ENGrid styles: v0.21.0
  *  ENGrid scripts: v0.21.0
