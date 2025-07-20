0a6da8089dc3e3444c67aab67097710655b48651
Fernando Santos <fern@ndo.io>
2025-07-19 22:43:35 -0400

    Payment Type Background Size

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Saturday, July 19, 2025 @ 22:31:06 ET
+ *  Date: Saturday, July 19, 2025 @ 22:41:20 ET
  *  By: fernando
  *  ENGrid styles: v0.22.4
  *  ENGrid scripts: v0.22.7
